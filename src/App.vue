2244d959f5ff9e80780c622ef7f47b120f42b7e5
Dan Koenig <dan.koenig@helloworld.com>
2021-06-19 12:54:46 -0400

    main navigation layout styled and scripted

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div v-if="menuOpen" class="blocker" @click="closeAllMenus"></div>
+      <div class="slide-menu game-menu" :class="[menuStates.game.state, {activated: menuStates.game.activated}]">
+        <div>Badges Rock</div>
+      </div>
+      <font-awesome-icon icon="star" class="nav-menu-icon star" :class="{activated: menuStates.main.activated}" @click="toggleMenu('game')" />
+      <div class="slide-menu main-menu" :class="[menuStates.main.state, {activated: menuStates.main.activated}]">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+      <font-awesome-icon icon="bars" class="nav-menu-icon hamburger" :class="{activated: menuStates.main.activated}" @click="toggleMenu('main')" />
     </div>
-    <router-view/>
+    <router-view class="inner-page"/>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from 'vuex';
 export default {
+    data () {
+      return {
+        menuStates:{
+          main: {
+            state: 'closed',
+            activated: false
+          },
+          game: {
+            state: 'closed',
+            activated: false
+          }
+        }
+      }
+    },
     methods: {
-      ...mapActions(['loadGames'])
+      ...mapActions(['loadGames']),
+      toggleMenu(menu){
+        this.menuStates[menu].state = this.menuStates[menu].state === 'closed' ? 'open' : 'closed';
+        if(this.menuStates[menu].state === 'open'){
+          for(let m in this.menuStates){
+            if(m !== menu){
+              this.menuStates[m].state = 'closed';
+            }
+          }
+        }
+      },
+      closeAllMenus(){
+        for(let m in this.menuStates){
+          this.menuStates[m].state = 'closed';
+        }
+      }
     },
     created () {
       this.loadGames();
+    },
+    computed: {
+      menuOpen () {
+        return Object.keys(this.menuStates).filter(item => this.menuStates[item].state === 'open').length > 0;        
+      }
+    },
+    mounted () {
+      setTimeout(() => {
+        for(let m in this.menuStates){
+          this.menuStates[m].activated = true;
+        }
+      },1000);
     }
 }
 </script>
 
 
-<style lang="css">
-@import "./scss/partials/_vicky.css";
+<style lang="scss">
+@import "./scss/default.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 Vue.config.productionTip = false
 
@@ -10,3 +13,8 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+library.add(faBars);
+library.add(faStar);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
